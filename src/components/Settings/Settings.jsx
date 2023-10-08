9dea0e7a2ee07d110cf327b50d34c4657d72ed5a
Prasanna Thapa <41299873+snowf0x@users.noreply.github.com>
2023-10-08 18:09:56 +0530

    Fixed Custom server functionality not working.

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,27 +1,30 @@
 import { useState } from 'react';
 import './settings.scss'
 import { Icon } from '@iconify/react';
-import { SERVER1, SERVER2, setHost, SELECTED_SERVER, HTTP_METHOD, API_HOST } from '../../Database/db';
+import { DEFAULT_SERVER, CUSTOM_SERVER, setHost, SELECTED_SERVER, HTTP_METHOD, API_HOST } from '../../Database/db';
 function Settings(props) {
     let savedServer = localStorage.getItem(SELECTED_SERVER);
-    if (savedServer != SERVER1 && savedServer != SERVER2) {
-        if (API_HOST.endsWith(SERVER1))
-            savedServer = SERVER1;
-        else
-            savedServer = SERVER2;
-    }
+    if (API_HOST.endsWith(DEFAULT_SERVER))
+        savedServer = DEFAULT_SERVER;
+    else
+        savedServer = CUSTOM_SERVER;
+    
     const [server, setServer] = useState(savedServer);
     function selectServer(host) {
         setHost(host);
-        localStorage.setItem(SELECTED_SERVER, host);
+        if(host !== DEFAULT_SERVER){
+            localStorage.setItem(SELECTED_SERVER, host);
+        } else {
+            localStorage.removeItem(SELECTED_SERVER);
+        }
         setServer(host);
     }
-    const [input, setInput] = useState(localStorage.getItem('custom_server') || SERVER2);
+    const [input, setInput] = useState(savedServer || DEFAULT_SERVER);
     return (
 
         <div>
             <div className="snip1265">
-                <div className={"plan " + (server == SERVER1 ? "featured" : "")}>
+                <div className={"plan " + (server === DEFAULT_SERVER ? "featured" : "")}>
                     <header><i><Icon icon="mdi:aws" color="#f39c12" /></i>
                         <h4 className="plan-title">
                             Primary Servers
@@ -29,7 +32,7 @@ function Settings(props) {
                         <div className="plan-cost"><span className="plan-type">host </span><span className="plan-price">prasannathapa.in</span></div>
                     </header>
                     <ul className="plan-features">
-                        <li><a style={{ fontFamily: 'monospace' }}>{HTTP_METHOD + SERVER1}</a>
+                        <li><a style={{ fontFamily: 'monospace' }}>{HTTP_METHOD + DEFAULT_SERVER}</a>
                         </li>
                         <li> Fastest feature updates (if any)
                         </li>
@@ -44,14 +47,14 @@ function Settings(props) {
                         <li> AWS Servers
                         </li>
                     </ul>
-                    {server == SERVER1 ?
+                    {server == DEFAULT_SERVER ?
                         <div className="plan-select"><a style={{ background: "black" }}>SELECTED</a></div>
                         :
-                        <div className="plan-select" onClick={() => selectServer(SERVER1)}><a>Select server</a></div>
+                        <div className="plan-select" onClick={() => selectServer(DEFAULT_SERVER)}><a>Select server</a></div>
                     }
                 </div>
 
-                <div className={"plan " + (server == SERVER2 ? "featured" : "")}>
+                <div className={"plan " + (server == CUSTOM_SERVER ? "featured" : "")}>
                     <header><i><Icon icon="la:node-js" color="#f39c12" /></i>
                         <h4 className="plan-title">
                             Custom Servers
@@ -59,7 +62,7 @@ function Settings(props) {
                         <div className="plan-cost"><span className="plan-type">host </span><span className="plan-price"><input id={'input'} value={input} onInput={e => setInput(e.target.value)} /></span></div>
                     </header>
                     <ul className="plan-features">
-                        <li><a style={{ fontFamily: 'monospace' }}>{HTTP_METHOD + SERVER2}</a>
+                        <li><a style={{ fontFamily: 'monospace' }}>{HTTP_METHOD + CUSTOM_SERVER}</a>
                         </li>
                         <li> Use your own server 
                         </li>
