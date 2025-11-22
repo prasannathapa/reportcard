5eb88468654106d28cdee7c84682e280c80a3873
Prasanna Thapa <admin@prasannathapa.in>
2025-11-22 16:16:59 +0530

    changed broken link and removed unused code

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,53 +9,7 @@ import studentComp from './student-comp.png'
 import multiRepImg from './multi-prev.png'
 import collegeAnalytiycs from './college-analytics.png'
 import subAnalytiycs from './sub-analytics.png'
-import { Area, AreaChart, Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
-function Home(props) {
-    const data = [
-        {
-            name: 'Page A',
-            uv: 4000,
-            pv: 2400,
-            amt: 2400,
-        },
-        {
-            name: 'Page B',
-            uv: 3000,
-            pv: 1398,
-            amt: 2210,
-        },
-        {
-            name: 'Page C',
-            uv: 2000,
-            pv: 9800,
-            amt: 2290,
-        },
-        {
-            name: 'Page D',
-            uv: 2780,
-            pv: 3908,
-            amt: 2000,
-        },
-        {
-            name: 'Page E',
-            uv: 1890,
-            pv: 4800,
-            amt: 2181,
-        },
-        {
-            name: 'Page F',
-            uv: 2390,
-            pv: 3800,
-            amt: 2500,
-        },
-        {
-            name: 'Page G',
-            uv: 3490,
-            pv: 4300,
-            amt: 2100,
-        },
-    ];
-
+function Home() {
     return (
         <div id="home">
 
@@ -66,7 +20,7 @@ function Home(props) {
                 <hr style={{ width: '10%', margin: '24px auto' }} />
                 <p style={{ margin: 'auto', color:"red"}}>IMPORTAMT NOTICE: MAKAUT CHANGED THEIR RESULT WEBSITE TO <a href="https://makaut.mastersofterp.in">MASTERSOFTERP</a>. IT WILL NOT WORK FOR RESULTS AFTER 2022, I WILL ADD OPTIONS FOR UPLOADING THE PDF DIRECTLY TO GET ANALYTICS</p>
                 <div className="flex">
-                    <img src={resultPreviewImg}></img>
+                    <img src={resultPreviewImg} alt="Result preview" />
                     <div style={{ display: 'inline-grid', margin: '32px 24px' }}>
                         <span>MAKAUT STUDENT REPORT</span>
                         <text>This is an analytics tool for your semester results as well as a rating card for your profile.
@@ -90,11 +44,11 @@ function Home(props) {
                         </text>
                     </div>
 
-                    <img src={subPreviewImg}></img>
+                    <img src={subPreviewImg} alt="Subject preview" />
 
                     <div style={{ height: '32px', width: '100%' }}></div>
 
-                    <img src={distImg}></img>
+                    <img src={distImg} alt="Distribution chart" />
                     <div style={{ display: 'inline-grid', margin: '32px 24px' }}>
                         <span>Semester Analytics</span>
                         <text>This is an analytics tool for your semester results! It works for most of the MAKAUT colleges and shows your results
@@ -105,14 +59,14 @@ function Home(props) {
                 <span className="">Multi Report</span>
                 <hr style={{ width: '10%', margin: '24px auto' }} />
                 <div className="flex">
-                    <img src={multiRepImg}></img>
+                    <img src={multiRepImg} alt="Multi report preview" />
                     <div style={{ display: 'inline-grid', margin: '32px 24px' }}>
                         <span>Get the whole class results!</span>
                         <text>Just put the beginning and ending roll number of any batch and the results are at
                             you finger tips! Sort the results by various parameters and find the Topper or the bottom ranker see your place amongst all</text>
                     </div>
                     <div style={{ height: '32px', width: '100%' }}></div>
-                    <img src={studentComp}></img>
+                    <img src={studentComp} alt="Student comparison chart" />
                     <div style={{ display: 'inline-grid', margin: '32px 24px' }}>
                         <span>Compare student on the go!</span>
                         <text>Add students from same or different institutes batches or even years! it will show you relevant parameters and charts to give you a brief comparison reliably and fast</text>
@@ -129,17 +83,17 @@ function Home(props) {
                             syllabus by the organization, question paper with out of syllabus questions or an excessively lengthy 
                             question paper and  lack of communication of students with teaching faculty</text>
                     </div>
-                    <img src={collegeAnalytiycs}></img>
+                    <img src={collegeAnalytiycs} alt="College analytics"></img>
                     <div style={{ display: 'inline-grid', margin: '32px 24px' }}>
                         <span>Subject performance for faculty!</span>
                         <text>Shows you the batch wise data for a particular subject for different colleges. Helps finding the average performance and subject teaching quality improvements each year</text>
                     </div>
-                    <img src={subAnalytiycs}></img>
+                    <img src={subAnalytiycs} alt="Subject analytics"></img>
                 </div>
             </header>
             <footer>
-                <span className="title">A project on <img className="rotate" src="https://github.com/prasannathapa/reportcard/raw/master/public/logo192.png"></img>
-                    and <img src="https://github.com/prasannathapa/reportcard/raw/master/public/nodejs.png"></img> by <a href="https://prasanna-thapa.herokuapp.com/" target="_blank">Prasanna Thapa</a></span>
+                <span className="title">A project on <img className="rotate" src="https://github.com/prasannathapa/reportcard/raw/master/public/logo192.png" alt="React Logo"></img>
+                    and <img alt='nodeJs' src="https://github.com/prasannathapa/reportcard/raw/master/public/nodejs.png"></img> by <a href="https://prasannathapa.in/" target="_blank" rel="noreferrer">Prasanna Thapa</a></span>
             </footer>
         </div>
     )
